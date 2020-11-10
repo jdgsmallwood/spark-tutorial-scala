280814da98e3d489bfdba9b14c4db67d8d47d3e2
Justin Smallwood <justin.d.smallwood@gmail.com>
2020-11-10 19:37:24 +1100

    Small update to brackets.

--- src/main/scala/com/learningspark/wordcount/WordCountExample.scala
+++ src/main/scala/com/learningspark/wordcount/WordCountExample.scala
@@ -6,7 +6,7 @@ import org.apache.spark.SparkContext._
 class WordCountExample extends App {
   // We need to create a Spark context to access the cluster.
   // The 'local' would be replaced by the address to the cluster.
-  val conf = new SparkConf().setMaster('local').setAppName("Example Application")
+  val conf = new SparkConf().setMaster("local").setAppName("Example Application")
   val sc = new SparkContext(conf)
 
 }
